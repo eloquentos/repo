d64437c0745623f3d5889864f9bb80630a1fc414
Chris Harvey <chris@chrisnharvey.com>
2016-01-10 14:59:57 +0000

    Move repo db symlink too

--- update_stable.sh
+++ update_stable.sh
@@ -3,9 +3,9 @@
 repo-add /tmp/stable.db.tar.gz /srv/arch/current/*/os/x86_64/*.pkg.tar.xz
 rm /srv/repo/stable/x86_64/*
 ln -s /srv/arch/current/*/os/x86_64/*.pkg.* /srv/repo/stable/x86_64/
-mv /tmp/stable.db.tar.gz /srv/repo/stable/x86_64/
+mv /tmp/stable.db* /srv/repo/stable/x86_64/
 
 repo-add /tmp/stable.db.tar.gz /srv/arch/current/*/os/i686/*.pkg.tar.xz
 rm /srv/repo/stable/i686/*
 ln -s /srv/arch/newest/*/os/i686/*.pkg.* /srv/repo/stable/i686/
-mv /tmp/stable.db.tar.gz /srv/repo/stable/i686
+mv /tmp/stable.db* /srv/repo/stable/i686
