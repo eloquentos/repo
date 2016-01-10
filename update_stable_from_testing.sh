3029c98763e52f47d9f3c3d66f2e72605b59935c
Chris Harvey <chris@chrisnharvey.com>
2016-01-10 17:36:59 +0000

    Adding logic to move our eloquent testing packages to stable

--- update_stable_from_testing.sh
+++ update_stable_from_testing.sh
@@ -3,11 +3,16 @@
 mv /srv/arch/current /srv/arch/old
 mv /srv/arch/newest /srv/arch/current
 mv /srv/repo/stable /srv/repo/old
+mv /srv/packages/stable /srv/packages/old
+mv /srv/packages/testing /srv/packages/stable
 rm /srv/repo/testing/*.pkg.*
 ln -s /srv/arch/current/*/os/x86_64/*.pkg.* /srv/repo/testing/x86_64/
 ln -s /srv/arch/current/*/os/i686/*.pkg.* /srv/repo/testing/i686/
+ln -s /srv/packages/stable/packages/*.pkg.* /srv/repo/testing/x86_64
+ln -s /srv/packages/stable/packages/*.pkg.* /srv/repo/testing/i686
 cp /srv/repo/testing /srv/repo/stable
 mv /srv/repo/stable/testing.db.tar.gz /srv/repo/stable/stable.db.tar.gz
 ln -sfn /srv/repo/stable/stable.db.tar.gz /srv/repo/stable/stable.db
 rm -rf /srv/arch/old
 rm -rf /srv/repo/old
+rm -rf /srv/packages/old
